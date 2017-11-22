c3b675e9b73763bda59030de6660d8b31a21df58
omokolataiwo <omokolataiwo@gmail.com>
2017-11-22 16:47:44 +0100

    create add center

--- server/index.js
+++ server/index.js
@@ -1,20 +1,16 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import adminRoute from './routes/admin';
 
 const app = express();
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-
-
-let count = 1;
-import centers from './models'
-import adminRoute from './routes/admin'
+app.use(bodyParser.urlencoded({ extended: false }));
 
 adminRoute(app);
-app.get('/', function(req, res) {
-	res.send("count is now >< " + centers.centers.one.name);
-})
+app.get('/', (req, res) => {
+  res.send('HELLO COME TO EVENT MANAGER');
+});
 
 app.listen(3000, () => {
-	console.log('Listening on 3000');
-})
+  console.log('Listening on 3000');
+});
