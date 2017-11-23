f9f804149de9f3c29111e04f1344916711a8f083
omokolataiwo <omokolataiwo@gmail.com>
2017-11-23 15:57:14 +0100

    add createEvent

--- server/controllers/event.js
+++ server/controllers/event.js
@@ -3,7 +3,17 @@ import Event from './_support/event';
 
 module.exports = {
   createEvent(req, res) {
-    
+    const event = new Event(req.body);
+    let events = database.events;
+    const newEventKey = database.keys.events;
+
+    event.validate();
+    if (!event.safe()) {
+      return res.json({ error: true, message: event.getErrors() });
+    }
+    events[newEventKey] = event.toJSON();
+    database.keys.events += 1;
+    return res.json({ error: false });
   },
   deleteEvent(req, res) {
    
