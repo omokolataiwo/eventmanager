4a5f79d01478bd4baf6f88f28b349d444fb997fe
omokolataiwo <omokolataiwo@gmail.com>
2017-11-23 15:50:15 +0100

    create route for event

---
+++ server/controllers/event.js
@@ -0,0 +1,14 @@
+import database from '../models';
+import Event from './_support/event';
+
+module.exports = {
+  createEvent(req, res) {
+    
+  },
+  deleteEvent(req, res) {
+   
+  },
+  editEvent(req, res) {
+    
+  },
+};
--- server/index.js
+++ server/index.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import adminRoute from './routes/admin';
+import eventRoute from './routes/event';
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 adminRoute(app);
+eventRoute(app);
 app.get('/', (req, res) => {
   res.send('HELLO COME TO EVENT MANAGER');
 });
