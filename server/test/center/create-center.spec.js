f2d5dbacbd09e2a4176aa6da9d916627d1c9ae14
Taiwo Kolawole <omokolataiwo@gmail.com>
2017-11-24 19:24:32 +0100

    Add create-center test spec

---
+++ server/test/center/create-center.spec.js
@@ -0,0 +1,30 @@
+'use strict'
+/*import chai from 'chai';
+import {chaiHttp} from 'chai-http';
+*/
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+const expect = chai.expect;
+import server from '../../index.js';
+
+chai.use(chaiHttp);
+
+describe('/centers', () => {
+	it('should create event center', (done) => {
+		let center =  {
+			capacity: 200,
+			amount: 2000,
+			"summary": ""
+		};
+		
+		chai.request(server)
+		.post('/centers')
+		.send(center)
+		.end((err, res) => {
+			expect(res).to.have.status(200);
+			expect(res).to.be.json;
+			expect(res.body).to.deep.equal({error:true});
+			done();
+		});
+	});
+})
