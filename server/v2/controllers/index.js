4a40f478a19cf35d729478cdce665ab0209a6d0c
Taiwo Kolawole <omokolataiwo@gmail.com>
2017-11-28 12:13:26 +0100

    Add api versioning

---
+++ server/v2/controllers/index.js
@@ -0,0 +1,7 @@
+import admin from './admin';
+import event from './event';
+
+module.exports = {
+  admin,
+  event,
+};
---
+++ server/v2/routes/admin.js
@@ -0,0 +1,8 @@
+import { admin } from '../controllers';
+
+module.exports = (app) => {
+  app.post('/centers', admin.createCenter);
+  app.get('/centers', admin.centers);
+  app.get('/centers/:id', admin.center);
+  app.put('/centers/:id', admin.editCenter);
+};
