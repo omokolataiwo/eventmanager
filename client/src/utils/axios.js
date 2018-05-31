c26eb7b56345f62a39c6f5f4a2071750bdb9ada9
TAIWO KOLAWOLE <omokolataiwo@gmail.com>
2018-05-31 10:39:02 +0100

    Update axios.js

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,12 @@
 import axios from 'axios';
 
+export const API_PATH =
+  process.env.NODE_ENV === 'development' ?
+    'http://localhost:5000/api/v2' :
+    'https://eventmanng.herokuapp.com/api/v2';
+
 const instance = axios.create({
-  baseURL: 'http://localhost:5000/api/v2'
+  baseURL: API_PATH
 });
 
 export default instance;
